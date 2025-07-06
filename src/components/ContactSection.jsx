020892cd8bed6993bdb0e49c4b83adfc00e05313
Adarsh singh <adarshsingh2412005@gmail.com>
2025-07-07 03:37:30 +0530

    .

--- src/components/ContactSection.jsx
+++ src/components/ContactSection.jsx
@@ -130,7 +130,7 @@ export const ContactSection = () => {
                   name="name"
                   required
                   className="w-full px-4 py-3 rounded-md border border-input bg-background focus:outline-hidden foucs:ring-2 focus:ring-primary"
-                  placeholder="Pedro Machado..."
+                  placeholder="Adarsh singh..."
                 />
               </div>
 
@@ -148,7 +148,7 @@ export const ContactSection = () => {
                   name="email"
                   required
                   className="w-full px-4 py-3 rounded-md border border-input bg-background focus:outline-hidden foucs:ring-2 focus:ring-primary"
-                  placeholder="john@gmail.com"
+                  placeholder="adarshsingh2412004@gmail.com"
                 />
               </div>
 
